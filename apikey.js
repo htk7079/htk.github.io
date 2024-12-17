1f18735d4a3381a11c89df73e7ebc4234422bdf9
htk7079 <155816861+htk7079@users.noreply.github.com>
2024-12-17 19:35:59 +0900

    Add files via upload

---
+++ apikey.js
@@ -0,0 +1,3 @@
+// apikey.js
+const apiKey = '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';
+module.exports = apiKey;
